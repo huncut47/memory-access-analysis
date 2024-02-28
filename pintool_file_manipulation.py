95baa25e74b8284961ab8f35d79d55e9085d014e
Rafael Rohal <92051602+huncut47@users.noreply.github.com>
2024-02-28 19:09:54 +0100

    added corrected version of the python script

--- pintool_file_manipulation.py
+++ pintool_file_manipulation.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-
-from __future__ import annotations
 import argparse
-
-PAGE_SIZE = 4096
 
 parser = argparse.ArgumentParser(description='Fragmentation analysis tool')
 parser.add_argument('filename')
@@ -12,20 +7,22 @@ parser.add_argument('-d', '--data', action='store_true', help='data fragmentatio
 parser.add_argument('-i', '--instr', action='store_true', help='instruction fragmentation')
 parser.add_argument('-a', '--all', action='store_true', help='fragmentation for all pages')
 
-def used_pages(file_location: str, pages_inst: dict, pages_data: dict, pages_total: dict) -> None:
+PAGE_SIZE = 4096
+
+def used_pages(file_location: str, pages_inst: dict[int, list[int]], pages_data: dict[int, list[int]], pages_total: dict[int, list[int]]) -> None:
     with open(file_location, 'r') as file:
         for line in file:
             if line == '#eof\n':
                 break
 
-            address, size, typ = line.strip().split(' ')
+            addr, size, typ = line.strip().split(' ')
 
             if typ == 'ip':
                 pages = pages_inst
             elif typ == 'addr':
                 pages = pages_data
 
-            address = int(address, 16)
+            address = int(addr, 16)
             page_number = address // PAGE_SIZE
 
             if page_number not in pages:
@@ -48,7 +45,7 @@ def used_pages(file_location: str, pages_inst: dict, pages_data: dict, pages_tot
                 pages[page_number][offset] = 1
                 pages_total[page_number][offset] = 1
 
-def fragmentation_calculation(output: list, pages_inst: dict, pages_data: dict, pages_total: dict) -> None:
+def fragmentation_calculation(output: list[str], pages_inst: dict[int, list[int]], pages_data: dict[int, list[int]], pages_total: dict[int, list[int]]) -> None:
     pages = pages_inst.keys() | pages_data.keys()
     used_total = 0
     used_data = 0
@@ -72,11 +69,11 @@ def fragmentation_calculation(output: list, pages_inst: dict, pages_data: dict, 
                 output.append(f'{page_number:#x}: v stranke boli aj data aj instrukcie')
 
         if args.all:
-            printline = f''
+            printline = ''
 
             if args.both:
                 if page_instr_fragm < 100 and page_data_fragm < 100:
-                    printline += f'! '
+                    printline += '! '
 
             printline += f'{page_number:#x} F: {page_total_fragm:.2f}%'
 
@@ -100,19 +97,13 @@ def fragmentation_calculation(output: list, pages_inst: dict, pages_data: dict, 
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    pages_inst = {}
-    pages_data = {}
-    pages_total = {}
-    output = []
+    pages_inst: dict[int, list[int]] = {}
+    pages_data: dict[int, list[int]] = {}
+    pages_total: dict[int, list[int]] = {}
+    output: list[str] = []
 
     used_pages(args.filename, pages_inst, pages_data, pages_total)
     fragmentation_calculation(output, pages_inst, pages_data, pages_total)
 
     for line in reversed(output):
         print(line)
-
-# /home/rafael/Desktop/Pinatrace/pinatrace.out
-
-# pridaj -d perhaps na debug
-# prida stranky ktore sa citali bez pisania
-# fragmentacia pre pamat / pre stranky s prepinacom
